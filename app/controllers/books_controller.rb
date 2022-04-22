c455816dbe5bb3d196cbe9df00abf4c8004ecf2f
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-04-22 11:08:23 +0000

    [add]ルーティング

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,15 @@ class BooksController < ApplicationController
   def show
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
   devise_for :users
+
+  get 'homes/top'
+  get 'homes/about'
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
