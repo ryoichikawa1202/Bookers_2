f757c78a4ac67138a6e0bf5d0239c6a33f2e7f79
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-04-23 10:54:31 +0000

    [add]header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'homes/top'
+  root to: 'homes#top'
+
   get 'homes/about'
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
