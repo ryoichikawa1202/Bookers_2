8bbb08d5c9cd77d04adc543925073660aeafdb69
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-04-25 08:59:40 +0000

    [add]book/index New book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
   def index
-  end
-
-  def show
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def show
   end
 
   def edit
@@ -15,5 +19,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
