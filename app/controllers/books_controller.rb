c205c34e14d3d18335bc4489f56a2b4e057fa5ff
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-05-02 09:20:06 +0000

    [add]show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,16 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.save
     redirect_to book_path(@book.id)
+    # if @book.save
+    #   redirect_to book_path(@book.id)
+    # else
+    #   @books = Book.all
+    #   render :index
+    # end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
