5ec8937282506307eb978e9d4be7e545bc2c7ee2
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-05-02 09:52:40 +0000

    [add]部分テンプレート

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,17 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
-  has_one_attached :image
+  has_one_attached :profile_image
+
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
+  validates :introduction, presence: false, length: {maximum: 50}
+
+  def get_profile_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+
+    profile_image.variant(resize: size).processed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   get 'homes/about'
-  resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
